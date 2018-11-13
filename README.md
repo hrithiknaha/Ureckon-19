f3662634608a4c3a1c68c98a2df37b22ec11583b
GaziTaufiqIslam <gzidev@gmail.com>
2018-11-13 20:37:09 +0530

    Gulp Template kit added

---
+++ README.md
@@ -0,0 +1,30 @@
+# Ureckon 2019 Website
+
+### Technologies used:
+  - Gulp
+  - Sass
+
+* _Things to do after cloning the repo_
+ - run `npm install`
+
+ Intalls the dev dependencies
+ - run `gulp`
+Running these two commands will install the dev dependencies mentioned in package.json. It will also launch the site with a localhost.
+Note: Both the commands might take several minutes to execute.*
+
+* Things to do after making changes
+  - run `gulp clean`
+
+  It will clean the dist folder, including the image cache. 
+  
+  To only delete the dist contents and keep the images run `clean:dist`
+
+  - run `gulp build`
+
+  This will rebuild the `dist` folder.
+
+  - Add, commit and push changes to git
+
+
+## Flexbox grid
+I have implemented a lightweight flexbox grid. Read the dcumentation from this website [flexboxgrid.com](flexboxgrid.com)
---
+++ gulpfile.js
@@ -0,0 +1,108 @@
+var autoprefixer = require('gulp-autoprefixer');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync');
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var del = require('del');
+var runSequence = require('run-sequence');
+
+// Basic Gulp task syntax
+gulp.task('hello', function() {
+  console.log('Hello Zell!');
+})
+
+// Development Tasks 
+// -----------------
+
+// Start browserSync server
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    }
+  })
+})
+
+gulp.task('sass', function() {
+  return gulp.src('app/scss/styles.scss') // Gets only the styles.scss file
+  .pipe(sourcemaps.init())  //Initializes the sourcemaps before the source files are piped through sass
+  .pipe(sass().on('error', sass.logError)) // Passes it through a gulp-sass, log errors to console
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],   //Automatically prefixes with vendors for the last two versions of every browser
+      cascade: false
+    }))
+    .pipe(sourcemaps.write(/*'./'*/))   //Writing external sourcemaps
+    .pipe(gulp.dest('app/css')) // Outputs it in the css folder
+    .pipe(browserSync.reload({ // Reloading with Browser Sync
+      stream: true
+    }));
+})
+
+// Watchers
+gulp.task('watch', function() {
+  gulp.watch('app/scss/**/*.+(scss|sass)', ['sass']);
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
+})
+
+// Optimization Tasks 
+// ------------------
+
+// Optimizing CSS and JavaScript 
+gulp.task('useref', function() {
+
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'));
+});
+
+// Optimizing Images 
+gulp.task('images', function() {
+  return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+    // Caching images that ran through imagemin
+    .pipe(cache(imagemin({
+      interlaced: true,
+    })))
+    .pipe(gulp.dest('dist/images'))
+});
+
+// Copying fonts 
+gulp.task('fonts', function() {
+  return gulp.src('app/fonts/**/*')
+    .pipe(gulp.dest('dist/fonts'))
+})
+
+// Cleaning 
+gulp.task('clean', function(cb) {
+   del.sync(['dist'], cb)
+})
+
+gulp.task('clean:dist', function() {
+  return del.sync(['dist/**/*', '!dist/images', '!dist/images/**/*']);
+});
+
+// Build Sequences
+// ---------------
+
+gulp.task('default', function(callback) {
+  runSequence(['sass', 'browserSync'], 'watch',
+    callback
+  )
+})
+
+gulp.task('build', function(callback) {
+  runSequence(
+    'clean:dist',
+    'sass',
+    ['useref', 'images', 'fonts'],
+    callback
+  )
+})
