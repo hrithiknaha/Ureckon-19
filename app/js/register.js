19b2846a03973967272e06d618121dcd53ed2655
Somsubhra Das <das.somsubhra1@gmail.com>
2019-01-22 23:41:31 +0530

    Added suggestion box for team members emails

--- app/js/register.js
+++ app/js/register.js
@@ -1,5 +1,11 @@
-var isSignedUp = true;
 var output;
+
+// FOR BACKEND DEVELOPER:
+
+var members = ["a@gmail.com", "b@gmail.com", "c@gmail.com", "d@gmail.com"]; // FETCH EMAILS FROM DATABASE AND ADD TO THIS ARRAY
+
+var isSignedUp = true; // GET USER SIGNED UP STATUS FROM DATABASE
+
 
 (function ($) {
     "use strict";
@@ -169,3 +175,38 @@ function notSignedUp() {
             </div>`;
     $("#output").html(output);
 }
+
+// typeahead:
+
+var substringMatcher = function (strs) {
+    return function findMatches(q, cb) {
+        var matches, substringRegex;
+
+        // an array that will be populated with substring matches
+        matches = [];
+
+        // regex used to determine if a string contains the substring `q`
+        substrRegex = new RegExp(q, 'i');
+
+        // iterate through the pool of strings and for any string that
+        // contains the substring `q`, add it to the `matches` array
+        $.each(strs, function (i, str) {
+            if (substrRegex.test(str)) {
+                matches.push(str);
+            }
+        });
+
+        cb(matches);
+    };
+};
+
+
+$('#the-basics .typeahead').typeahead({
+    hint: true,
+    highlight: true,
+    minLength: 1
+},
+    {
+        name: 'states',
+        source: substringMatcher(members)
+    });
